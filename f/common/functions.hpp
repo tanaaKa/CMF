0915d6cdc2f3826af5ba3d860d506b011e5ecaeb
tanaaKa <tanaka_s2@yahoo.com>
2017-08-07 02:56:23 -0500

    Moving Spec and Respawn to modpack

--- f/common/functions.hpp
+++ f/common/functions.hpp
@@ -24,30 +24,4 @@ class F // Defines the "owner"
 		class mapClickTeleportUnit{};
 		class mapClickTeleportGroup{};
 	};
-	class fspectator
-	{
-		file = "f\spect";
-		class CamInit{};
-		class OnUnload{};
-		class DrawTags{};
-		class EventHandler{};
-		class FreeCam{};
-		class GetPlayers{};
-		class ReloadModes{};
-		class UpdateValues{};
-		class HandleCamera{};
-		class ToggleGUI{};
-		class OnMapClick{};
-		class DrawMarkers{};
-		class ForceExit{};
-		class HandleMenu{};
-		class showMenu{};
-	};
-	class frespawn {
-		file = "f\respawn";
-		class RespawnDrawMapIcons {};
-		class RespawnLocalClient {};
-		class RespawnWaveServer {};
-		class RespawnInit { postInit = 1; };
-	};
 };
